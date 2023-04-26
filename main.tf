84ca89dedf02171c469275cd8feb7dad38d90762
vhegde <vhegde@teksystems.com>
2023-04-26 10:28:22 +0530

    test

--- main.tf
+++ main.tf
@@ -1,11 +1,17 @@
-module "aws_s3_bucket" {
-  source                = "./modules"
-  region                = var.region
-  bucket_name           = var.bucket_name
-  tags                  = var.tags
-  version_status        = var.version_status
-  cors_configuration    = var.cors_configuration
-  encryption_configuration = var.encryption_configuration
-  bucket_logging           = var.bucket_logging
+variable "bucket_name" {}
+variable "Environment" {}
+
+terraform {
+  backend "s3" {
   }
-  
+}
+
+resource "aws_s3_bucket" "first"{
+
+  bucket = var.bucket_name
+
+  tags = {
+    Environment = var.Environment
+  }
+
+}
